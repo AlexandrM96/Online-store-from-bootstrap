27831631fc22d37c16125e04dd65045600c36efd
AlexandrM96 <super.dwtd2013@yandex.ru>
2022-02-19 12:26:28 +0300

    добавил рейтинг и исправил верстку

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,14 @@
 let cardName = document.querySelectorAll('.card-title');
 let cardImage = document.querySelectorAll('.card-img-top');
 let cardPrice = document.querySelectorAll('.card-price');
+let contStar = document.querySelectorAll('.small');
 let url = `https://fakestoreapi.com/products`;
 
 function apiRequest() {
     fetch(url)
     .then(res => res.json())
     .then(data => {
-        // console.log(data);
+        console.log(data);
         for(let i = 0; i < data.length; i++) {
             for(let i = 0; i < cardName.length; i++) {
                 cardName[i].innerText = data[i].title;
@@ -17,6 +18,11 @@ function apiRequest() {
             };
             for(let i = 0; i < cardPrice.length; i++) {
                 cardPrice[i].innerText = `$${data[i].price}`;
+            };
+            for(let j = 0; j < data[i].rating.rate; j++) {
+               let d = document.createElement('div');
+               d.className = 'bi-star-fill';
+                contStar[i].append(d);
             };
         };
     }); 
